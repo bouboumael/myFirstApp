682d9487e15988eebe6ccd5d6f81a519e05455e6
maelchariault <bouboumael@gmail.com>
2023-08-06 00:20:00 +0200

    Initial Commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by Mael Chariault on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
